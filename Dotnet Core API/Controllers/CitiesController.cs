f5dca8cef38b8fc00b0f545736093791220a62d1
DhruvKinger <cse1785@gmail.com>
2020-07-02 21:52:49 +0530

    add files

---
+++ Dotnet Core API/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCore.API.Controllers
+{  
+    [ApiController]
+   public class CitiesController:ControllerBase
+ {
+
+        public JsonResult GetCities()
+        {
+
+            return new JsonResult(
+
+                new List<Object>
+                {
+                    new{id=1,Name="Mlk"},
+                    new{id=2,Name="Chandigarh"}
+                }
+            );
+        }
+
+ }
+
+}
